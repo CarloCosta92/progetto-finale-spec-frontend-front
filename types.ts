504844d345d7a0da415a48f62831cf1bdafe0a83
Carlo Costa <carlo.costa92@gmail.com>
2025-08-29 10:33:00 +0200

    feat: push finale

---
+++ types.ts
@@ -0,0 +1,21 @@
+export type Car = {
+    title: string;
+    brand: string;
+    model: string;
+    category: string;
+    price: string;
+    color: string;
+    img: string;
+    year: number;
+    mileage: string;
+    fuelType: string;
+    transmission: string;
+    power: string;
+    doors: number;
+    seats: number;
+    emissions: string;
+    consumption: string;
+    warranty: string;
+    location: string;
+    condition: string;
+}
